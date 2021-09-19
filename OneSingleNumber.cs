dda65fcd103e043ead301a279a2fefe007f82281
sagomanoj <33486909+sagomanoj@users.noreply.github.com>
2021-09-19 12:54:01 +0530

    Add files via upload
    Find the Single Element in array.

---
+++ OneSingleNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class OneSingleNumber
+    {
+        //Given an Array of elements, all the elements in the array are repeated twice except an element. Find the single number.
+
+        public int SingleNumber(int[] array)
+        {
+            int result = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                result = array[i] ^ result;
+            }
+
+            return result;
+        }
+    }
+}
